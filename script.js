8b5528f54d58c17b9ea41e2a1485b578796b6a7b
Aaron <awitthohn@yahoo.com>
2023-02-13 11:34:30 -0500

    initial question/prompt for user, (password length) achieved.

--- script.js
+++ script.js
@@ -10,5 +10,30 @@ function writePassword() {
 
 }
 
+var upperCase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+var lowerCase = ["abcdefghijklmnopqrstuvwxyz"];
+var numbers = ["0123456789"];
+var specialChar = ["!#$%&)( *+,.;:=-><?\\^_~`|/"]
+var length = prompt("How long would you like your password to be? (It must be atleast 8 characters and no longer than 128.)")
+
+
+
+function generatePassword() {
+  var password = "";
+  var length = prompt("How long would you like your password to be? (It must be atleast 8 characters and no longer than 128.)")
+
+  if(  length < 8 ||  length > 128){
+    alert("Password length must be at least 8 characters and no greater than 128!")
+    var length = prompt("How long would you like your password to be? (It must be atleast 8 characters and no longer than 128.)");
+  }
+}
+
+
+
+
+
+
+
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
